6acce9aa06bc57cf6be6cc696c56897705cb551d
debashis1992 <d2011das@gmail.com>
2021-11-27 11:40:36 +0530

    adding options

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pytube import YouTube, StreamQuery
 
-link = input("Enter link:")
+link = input("Enter link: ")
 yt = YouTube(link)
 
 print(f"Title: {yt.title}")
@@ -20,8 +20,6 @@ for stream in audio_streams_li:
     print(stream)
 
 
-
-
 def download(streams: StreamQuery, itag: int = None, type: str = None):
     try:
         if streams is None:
@@ -29,10 +27,26 @@ def download(streams: StreamQuery, itag: int = None, type: str = None):
         if type == "audio":
             ys = streams.filter(type="audio").get_by_itag(itag)
         else:
-            ys = streams.filter(type="video").get_by_itag(itag)
+            ys = streams.filter(type="video", progressive=True).get_by_itag(itag)
 
-        ys.download()
+        ys.download(output_path="./downloads/")
     except Exception as e:
         print(e)
 
-download(yt.streams, 140, "audio")
+
+audio_codec = input("Enter only audio (Y/N): ") == "Y"
+format_type = None
+if audio_codec:
+    format_type = "audio"
+    itag = audio_streams_li.filter(abr = "128kbps")[0].itag
+else:
+    format_type = "video"
+    available_formats = video_streams_li.filter(progressive=True)
+    print(f"Available resolutions: {available_formats}")
+    res_dict = {index:format for index,format in enumerate(available_formats)}
+    print(res_dict)
+    res_index = int(input("Enter resolution index: "))
+    res = res_dict[res_index]
+    itag = res.itag
+
+download(yt.streams, itag, format_type)
